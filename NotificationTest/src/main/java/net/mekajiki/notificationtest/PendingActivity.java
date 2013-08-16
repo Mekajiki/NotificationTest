a67d99d99b8dab6b96ed985981171555014e4ba9
Yuichiro NAKAGAWA <ii.hsif.drows@gmail.com>
2013-08-16 15:21:27 +0900

    sets background black

--- NotificationTest/src/main/java/net/mekajiki/notificationtest/PendingActivity.java
+++ NotificationTest/src/main/java/net/mekajiki/notificationtest/PendingActivity.java
@@ -1,10 +1,13 @@
 package net.mekajiki.notificationtest;
 
+import android.*;
+import android.R;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,6 +16,9 @@ public class PendingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().getDecorView().setBackgroundColor(R.color.black);
 
         new AlertDialog.Builder(this)
                 .setTitle("Notification")
